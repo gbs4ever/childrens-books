3375a4a56ec099b2da83b53a6557b6222a0fb5da
gbs4ever <gbs4ever2@gmail.com>
2019-03-11 17:33:55 -0400

    finished books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,8 +3,7 @@ class BooksController < ApplicationController
   # GET: /books
   get "/books" do
    @books =Book.all
-  
-    erb :"/books/index.html"
+  erb :"/books/index.html"
   end
 
   # GET: /books/new
@@ -15,27 +14,34 @@ class BooksController < ApplicationController
   # POST: /books
   post "/books" do
   Book.create(params)
-    
-    redirect "/books"
+  redirect "/books"
   end
 
   # GET: /books/5
   get "/books/:id" do
+    @book = Book.find_by(params[:id])
     erb :"/books/show.html"
   end
 
   # GET: /books/5/edit
   get "/books/:id/edit" do
-    erb :"/books/edit.html"
+    
+  @book = Book.find_by(params[:id])
+  erb :"/books/edit.html"
   end
 
   # PATCH: /books/5
   patch "/books/:id" do
-    redirect "/books/:id"
+   
+    var = Book.find_by(params[:id])
+    params.delete("_method")
+    var.update(params) 
+    redirect "/books/#{var.id}"
   end
 
   # DELETE: /books/5/delete
   delete "/books/:id/delete" do
+   Book.find(params[:id]).delete
     redirect "/books"
   end
 end
