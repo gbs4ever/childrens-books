7999437806b6447679dd609ef2a0d79aa880d687
gbs4ever <gbs4ever2@gmail.com>
2019-03-11 02:22:32 -0400

    built user and books db

--- db/migrate/20190311061422_create_books.rb
+++ db/migrate/20190311061422_create_books.rb
@@ -1,7 +1,10 @@
 class CreateBooks < ActiveRecord::Migration
   def change
     create_table :books do |t|
-
+      t.string  :author
+      t.string  :title
+      t.string  :status
+      t.text     :comments
       t.timestamps null: false
     end
   end
