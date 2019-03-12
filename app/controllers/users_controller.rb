04f29b32c7a929efdbc05e196cbe94cd2d29b22a
gbs4ever <gbs4ever2@gmail.com>
2019-03-12 00:52:29 -0400

    fixed user bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,37 +1,74 @@
 class UsersController < ApplicationController
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
 
-  # GET: /users
+  #GET: /users  200
   get "/users" do
-    erb :"/users/index.html"
+   erb :"/users/index.html"
   end
 
-  # GET: /users/new
-  get "/users/new" do
-    erb :"/users/new.html"
+  # GET: /users/new   200
+  get "/users/signup" do
+    erb :"/users/signup.html"
   end
 
-  # POST: /users
+  # POST: /users    200
   post "/users" do
-    redirect "/users"
+    user = User.new(params)
+    if user.save
+      redirect "/users/login"
+    else
+      redirect "/users"
+    end
   end
+  
+  #GET: /users/login  200
+  get "/users/login" do
+		erb :"/users/login.html"
+	end
 
-  # GET: /users/5
-  get "/users/:id" do
+  # POST: /users    200
+  post "/login" do
+    user = User.find_by(:email => params[:email])
+   if user  && user.authenticate(params[:password])
+     session[:user_id] = user.id
+     redirect "/users/#{user.id}"
+   else
+    redirect "/users"
+   end
+ end
+    get "/logout" do
+      session.clear
+      redirect "/users"
+    end
+
+
+
+
+  # GET: /users/5 200
+ get "/users/:id" do
+  @user = User.find_by(params[:id])
+    if !!session[:user_id]
     erb :"/users/show.html"
+    else
+    redirect "/users"
+    end
   end
 
   # GET: /users/5/edit
-  get "/users/:id/edit" do
-    erb :"/users/edit.html"
-  end
+  #get "/users/:id/edit" do
+   # erb :"/users/edit.html"
+  #end
 
   # PATCH: /users/5
-  patch "/users/:id" do
-    redirect "/users/:id"
-  end
+  #patch "/users/:id" do
+   # redirect "/users/:id"
+  #end
 
   # DELETE: /users/5/delete
-  delete "/users/:id/delete" do
-    redirect "/users"
-  end
+  #delete "/users/:id/delete" do
+   # redirect "/users"
+  #end
 end
